1c309e5de49567508de951c93fba3ea279c7ebe1
100larczyk <100larczyk.m@gmail.com>
2023-02-22 13:08:53 +0100

    added eventListener for each Tile. Enclosed it in Game Module

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 const createGameZone = (() => {
   const gameBoardSpace = document.querySelector(".gameBoardSpace");
-
   const gameBoardArray = [];
+
   const rows = 3;
   const columns = 3;
-
-  console.log(gameBoardArray);
 
   function createGameTile() {
     const tile = document.createElement("div");
     tile.className = "tile";
     gameBoardSpace.appendChild(tile);
-
     return tile;
   }
+
   const createGameBoardTiles = () => {
     for (let i = 0; i < rows; i++) {
       gameBoardArray[i] = [];
       for (let j = 0; j < columns; j++) {
-        gameBoardArray[i][j] = 0;
-        createGameTile();
+        gameBoardArray[i][j] = createGameTile();
       }
     }
   };
-
-  return { createGameBoardTiles };
+  console.log(gameBoardArray);
+  return { createGameBoardTiles, gameBoardArray };
 })();
 
-const Player = (name, mark, points, e) => {
-  return { name, mark, points };
+const Player = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
 };
 
-let PlayerOne = Player("Player One", "x", 0);
-let PlayerTwo = Player("Player Two", "o", 0);
+const PlayerOne = Player("Player One", "X");
+const PlayerTwo = Player("Player Two", "O");
 
 const Game = (() => {
   createGameZone.createGameBoardTiles();
+
+  let activePlayer = PlayerOne;
+
+  createGameZone.gameBoardArray.forEach((row) => {
+    row.forEach((tile) => {
+      tile.addEventListener("click", (e) => {
+        e.target.textContent = activePlayer.mark;
+        activePlayer = activePlayer === PlayerOne ? PlayerTwo : PlayerOne;
+      });
+    });
+  });
 })();
